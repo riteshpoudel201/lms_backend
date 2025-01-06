340a2670cafa727114e0a8a7896cd3017a8e9f15
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-06 21:09:10 +0545

    added email temaplate and email service for sending notifications after user account is activated

--- src/services/email/emailService.js
+++ src/services/email/emailService.js
@@ -1,8 +1,13 @@
-import { userActivationLinkTemplate } from "./emailTemplate.js";
+import { userActivatedEmailTemplate, userActivationLinkTemplate } from "./emailTemplate.js";
 import { emailTransporter } from "./transport.js";
 
 export const userActivationLink = async (obj) => {
   const transporter = emailTransporter();
   const info = await transporter.sendMail(userActivationLinkTemplate(obj));
+  return info.messageId;
+};
+export const userActivatedLoginLink = async (obj) => {
+  const transporter = emailTransporter();
+  const info = await transporter.sendMail(userActivatedEmailTemplate(obj));
   return info.messageId;
 };
--- src/services/email/emailTemplate.js
+++ src/services/email/emailTemplate.js
@@ -17,3 +17,24 @@ Library Management System
   };
   return message;
 };
+
+
+export const userActivatedEmailTemplate = ({ email, name, url }) => {
+  const message = {
+    from: `"Local Library" <${process.env.SMTP_EMAIL}>`,
+    to: email,
+    subject: "Action Required - Activate your new account.",
+    text: `Hello ${name}, your account has been successfully activated and you may proceed to login using url below./n ${url}`,
+    html: `<p>Hello , ${name}</p>
+<br/>
+<p>Your account has been activated. Click the button below to login to your account.</p>
+<a href=${url}>
+<button style="background: green; color:white; padding: 2rem 1rem">Activate Now</button></a>
+<br/>
+<br/>
+With Regards,
+Library Management System
+`,
+  };
+  return message;
+};
