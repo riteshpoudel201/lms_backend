9825be8ca43c56da74dc0c11d08ae5d2eb8f1cc6
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-06 21:08:07 +0545

    added routes to activate user

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { insertNewUser } from "../controllers/authController.js";
+import { activateUser, insertNewUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", insertNewUser);
+router.post("/activate-user", activateUser);
 
 export default router;
