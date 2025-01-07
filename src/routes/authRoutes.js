d27b6e2d9236f78da73eb3b07b5737e7cd964338
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-07 17:31:47 +0545

    added validation middleware to check data before it is processed.

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { activateUser, insertNewUser } from "../controllers/authController.js";
+import { validateUser, validateUserActivationData } from "../middlewares/validation/authValidation.js";
 
 const router = express.Router();
 
-router.post("/register", insertNewUser);
-router.post("/activate-user", activateUser);
+router.post("/register",validateUser, insertNewUser);
+router.post("/activate-user",validateUserActivationData, activateUser);
 
 export default router;
