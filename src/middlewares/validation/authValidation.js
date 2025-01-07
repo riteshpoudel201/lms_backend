849afbabd04af9fcafaf10f5c064e174c3db72e5
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-07 17:31:17 +0545

    used validator to validate the incoming data related to user

---
+++ src/middlewares/validation/authValidation.js
@@ -0,0 +1,19 @@
+import { validateData } from "./joiValidation.js";
+
+export const validateUser = (req, res, next) => {
+  const obj = {
+    firstName: Joi.string().min(3).required(),
+    lastName: Joi.string().min(3).required(),
+    phone: Joi.number().min(9).optional(),
+    email: Joi.string().email({ minDomainSegments: 2 }).required(),
+    password: Joi.string().required(),
+  };
+  return validateData({ req, res, next, obj });
+};
+export const validateUserActivationData = (req, res, next) => {
+  const obj = {
+    sessionId: Joi.string().min(10).max(50).required(),
+    t: Joi.string().min(10).max(50).required(),
+  };
+  return validateData({ req, res, next, obj });
+};
