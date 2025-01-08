56ad2f110ce825152473063a711963aad3149411
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-08 16:55:52 +0545

    added method to compare password

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -4,3 +4,7 @@ const SALT_ROUNDS = 15
 export const hashPassword = (plainPassword) =>{
     return bcrypt.hashSync(plainPassword, SALT_ROUNDS)
 }
+
+export const comparePassword = (plainPassword, hashedPassword) => {
+    return bcrypt.compareSync(plainPassword, hashedPassword);
+}
