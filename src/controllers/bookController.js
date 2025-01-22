32808ebf454f683e0da061f90f74b50a85c7020d
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-22 17:25:08 +0545

    added book controller for book routes

---
+++ src/controllers/bookController.js
@@ -0,0 +1,79 @@
+import { responseClient } from "../middlewares/responseClient.js";
+import { createBook, getAllBook } from "../models/book/bookModel.js";
+
+export const getAllBooks = async (req, res, next) => {
+  const { role } = req.userInfo;
+  if (role !== "admin") {
+    return responseClient({
+      req,
+      res,
+      message: "User unauthorized.",
+      statusCode: 401,
+    });
+  }
+  try {
+    const books = await getAllBook();
+    return responseClient({ req, res, message: "Books fetched successfully." });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAllAvailableBooks = async (req, res, next) => {
+  const user = req.userInfo;
+  if (user?._id) {
+    return responseClient({
+      req,
+      res,
+      message: "User unauthorized.",
+      statusCode: 401,
+    });
+  }
+  try {
+    const books = await getAllBook();
+    const availableBooks = books.filter((book) => book.available === true);
+    return responseClient({
+      req,
+      res,
+      message: "Books fetched successfully.",
+      data: availableBooks,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const insertNewBook = async (req, res, next) => {
+  const user = req.userInfo;
+  if (user.role !== "admin") {
+    return responseClient({
+      req,
+      res,
+      message: "User unauthorized.",
+      statusCode: 401,
+    });
+  }
+  try {
+    const newBook = req.body;
+    const book = await createBook({
+      ...newBook,
+      addedBy: {
+        name: user.firstName + " " + user.lastName,
+        adminId: user._id,
+      },
+    });
+    if (book?._id) {
+      return responseClient({
+        req,
+        res,
+        message: "Book created successfully.",
+      });
+    }
+    return responseClient({
+      req,
+      res,
+      message: "Unable to create book.",
+      statusCode: 400,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
