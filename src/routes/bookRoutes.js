c63c325ad233945c153d7794b9a9f9c13b7c2471
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-23 16:39:49 +0545

    added admin check method while inserting new book

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getAllAvailableBooks, getAllBooks, insertNewBook } from "../controllers/bookController.js";
 import { validateBook } from "../middlewares/validation/bookValidation.js";
+import { adminAuthMiddleware, userAuthMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/books",userAuthMiddleware ,getAllBooks);
-router.get("/books/available",userAuthMiddleware ,getAllAvailableBooks);
+router.get("/",userAuthMiddleware ,getAllBooks);
+router.get("/available" ,getAllAvailableBooks);
 
-router.post("/books", validateBook, insertNewBook)
+router.post("/", userAuthMiddleware,adminAuthMiddleware ,validateBook, insertNewBook)
 
 
 export default router;
