176546d63d8682725838a497744b226e84d30c7f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-26 20:14:08 +0545

    modified the data validating code to accept data from params as well

--- src/middlewares/validation/joiValidation.js
+++ src/middlewares/validation/joiValidation.js
@@ -1,11 +1,11 @@
 import Joi from "joi";
 import { responseClient } from "../responseClient.js";
-export const validateData = ({ req, res, next, obj }) => {
+export const validateData = ({ req, res, next, obj, source }) => {
   //creating schema for validation purpose
   const schema = Joi.object(obj);
 
   //validating the incoming data using the schema
-  const value = schema.validate(req.body);
+  const value = schema.validate(req[source] || req.body);
   if (value.error) {
     return responseClient({
       req,
