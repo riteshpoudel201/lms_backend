5f4284c643dbdcac6d2eeca439e566550c334a5e
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-26 20:14:37 +0545

    added routes for updating book

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,14 +1,39 @@
 import express from "express";
-import { getAllAvailableBooks, getAllBooks, insertNewBook } from "../controllers/bookController.js";
-import { validateBook } from "../middlewares/validation/bookValidation.js";
-import { adminAuthMiddleware, userAuthMiddleware } from "../middlewares/authMiddleware.js";
+import {
+  getAllAvailableBooks,
+  getAllBooks,
+  insertNewBook,
+  updateExistingBook,
+} from "../controllers/bookController.js";
+import {
+  validateBook,
+  validateBookID,
+} from "../middlewares/validation/bookValidation.js";
+import {
+  adminAuthMiddleware,
+  userAuthMiddleware,
+} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/",userAuthMiddleware ,getAllBooks);
-router.get("/available" ,getAllAvailableBooks);
+router.get("/", userAuthMiddleware, getAllBooks);
+router.get("/available", getAllAvailableBooks);
 
-router.post("/", userAuthMiddleware,adminAuthMiddleware ,validateBook, insertNewBook)
+router.post(
+  "/",
+  userAuthMiddleware,
+  adminAuthMiddleware,
+  validateBook,
+  insertNewBook
+);
 
+router.put(
+  "/:id",
+  userAuthMiddleware,
+  adminAuthMiddleware,
+  validateBookID,
+  validateBook,
+  updateExistingBook
+);
 
 export default router;
