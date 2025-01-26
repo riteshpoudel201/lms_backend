e53543c85feb8a8be322bf62b8063d6776c3b2c7
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-26 20:13:48 +0545

    created validating book id method from params

--- src/middlewares/validation/bookValidation.js
+++ src/middlewares/validation/bookValidation.js
@@ -1,21 +1,19 @@
+import Joi from "joi";
+import { validateData } from "./joiValidation";
+
+export const validateBookID = (req, res, next) => {
+  const obj = {
+    id: Joi.string().required(),
+  };
+  return validateData({ req, res, next, obj, source:'params' });
+};
 export const validateBook = (req, res, next) => {
   const obj = {
-    status: Joi.string().optional(),
     title: Joi.string().min(3).required(),
     year: Joi.number().min(9).required(),
     imageURL: Joi.string().email({ minDomainSegments: 2 }).required(),
     isbn: Joi.string().required(),
     genre: Joi.string().required(),
-    available: Joi. string(),
-    averageRating: Joi.number().optional(),
-    addedBy:Joi.object({
-        name:Joi.string(),
-        adminId: Joi.string(),
-    }),
-    lastUpdatedBy:Joi.object({
-        name:Joi.string(),
-        adminId: Joi.string(),
-    }),
     author: Joi.string().required()
   };
   return validateData({ req, res, next, obj });
