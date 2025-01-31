5095ad76340c1867cac715661d4834b9de5e953a
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-01-31 13:37:28 +0545

    added description field and pattern for providing valid isbn number

--- src/middlewares/validation/bookValidation.js
+++ src/middlewares/validation/bookValidation.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { validateData } from "./joiValidation";
+import { validateData } from "./joiValidation.js";
 
 export const validateBookID = (req, res, next) => {
   const obj = {
@@ -12,9 +12,12 @@ export const validateBook = (req, res, next) => {
     title: Joi.string().min(3).required(),
     year: Joi.number().min(9).required(),
     imageURL: Joi.string().email({ minDomainSegments: 2 }).required(),
-    isbn: Joi.string().required(),
+    isbn: Joi.string().pattern(/^\d{10}$|^\d{13}$/).messages({
+      "string.pattern.base": "ISBN is not in valid format."
+    }),
     genre: Joi.string().required(),
-    author: Joi.string().required()
+    author: Joi.string().required(),
+    description: Joi.string().required(),
   };
   return validateData({ req, res, next, obj });
 };
