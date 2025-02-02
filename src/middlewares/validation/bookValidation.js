e625810afbb138988ff95c9ab385de7b8afeb82c
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-02 17:12:01 +0545

    fixed type of imageurl

--- src/middlewares/validation/bookValidation.js
+++ src/middlewares/validation/bookValidation.js
@@ -11,7 +11,7 @@ export const validateBook = (req, res, next) => {
   const obj = {
     title: Joi.string().min(3).required(),
     year: Joi.number().min(9).required(),
-    imageURL: Joi.string().email({ minDomainSegments: 2 }).required(),
+    imageURL: Joi.string().uri().required(),
     isbn: Joi.string().pattern(/^\d{10}$|^\d{13}$/).messages({
       "string.pattern.base": "ISBN is not in valid format."
     }),
