83d7fa50b2e42e9108d5850f8fa80cb87fdc128f
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-18 19:35:02 +0545

    validate incoming data for updating book

--- src/middlewares/validation/bookValidation.js
+++ src/middlewares/validation/bookValidation.js
@@ -7,6 +7,7 @@ export const validateBookID = (req, res, next) => {
   };
   return validateData({ req, res, next, obj, source:'params' });
 };
+
 export const validateBook = (req, res, next) => {
   const obj = {
     title: Joi.string().min(3).required(),
@@ -18,6 +19,20 @@ export const validateBook = (req, res, next) => {
     genre: Joi.string().required(),
     author: Joi.string().required(),
     description: Joi.string().required(),
+  };
+  return validateData({ req, res, next, obj });
+};
+
+export const validateUpdateBook = (req, res, next) => {
+  const obj = {
+    title: Joi.string().min(3).required(),
+    year: Joi.number().min(9).required(),
+    imageURL: Joi.string().uri().required(),
+    genre: Joi.string().required(),
+    author: Joi.string().required(),
+    description: Joi.string().required(),
+    expectedAvailabilityDate: Joi.date().allow(null, ""),
+    status: Joi.string().valid("active","inactive").required(),
   };
   return validateData({ req, res, next, obj });
 };
