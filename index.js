cfef2f81f0711eab304b4f502ab4d53d974f93c6
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-23 18:26:16 +0545

    modified the code to accept url encoded data

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ dotenv.config();
 const port = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(morgan("dev"));
 app.get("/", (req,res)=>{
