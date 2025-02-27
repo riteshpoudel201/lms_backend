74ac7b33f6d69c1c9e2cbedb5fa9390d27be04fb
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2025-02-27 17:33:40 +0545

    removed imageURL validation and added bookImage

--- src/middlewares/validation/bookValidation.js
+++ src/middlewares/validation/bookValidation.js
@@ -12,7 +12,7 @@ export const validateBook = (req, res, next) => {
   const obj = {
     title: Joi.string().min(3).required(),
     year: Joi.number().min(9).required(),
-    imageURL: Joi.string().uri().required(),
+    bookImage: Joi.object(),
     isbn: Joi.string().pattern(/^\d{10}$|^\d{13}$/).messages({
       "string.pattern.base": "ISBN is not in valid format."
     }),
@@ -27,7 +27,6 @@ export const validateUpdateBook = (req, res, next) => {
   const obj = {
     title: Joi.string().min(3).required(),
     year: Joi.number().min(9).required(),
-    imageURL: Joi.string().uri().required(),
     genre: Joi.string().required(),
     author: Joi.string().required(),
     description: Joi.string().required(),
