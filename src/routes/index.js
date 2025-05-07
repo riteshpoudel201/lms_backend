32fd8fcd1b92775a48d8f5d160a0441bcf7990ef
Ritesh Poudel <ritesh.poudel.34@gmail.com>
2025-05-07 18:00:38 +0545

    added in the index for easy import

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,4 @@
 export {default as authRouter} from "./authRoutes.js"
 export {default as userRouter} from "./userRoutes.js"
 export {default as bookRouter} from "./bookRoutes.js"
+export {default as borrowRouter} from "./borrowedBookRoutes.js"
